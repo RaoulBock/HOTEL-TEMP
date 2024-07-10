a5abeee4cea809bf4bb02507a0b66e8ac38ba482
Raoul Bock <raoulb@deepcatch.com.na>
2024-07-10 16:15:31 +0200

    changes

--- src/Screens/RoomScreen.js
+++ src/Screens/RoomScreen.js
@@ -60,13 +60,15 @@ export const RoomScreen = () => {
             return (
               <div key={index} className="room">
                 <img src={room.img} alt={room.name} />
-                <h1>${room.price}.00</h1>
-                <h5>{room.name}</h5>
-                <p>
-                  {room.description.length > 80
-                    ? `${room.description.substring(0, 80)}...`
-                    : room.description}
-                </p>
+                <div>
+                  <h1>${room.price}.00</h1>
+                  <h5>{room.name}</h5>
+                  <p>
+                    {room.description.length > 80
+                      ? `${room.description.substring(0, 80)}...`
+                      : room.description}
+                  </p>
+                </div>
               </div>
             );
           })}
